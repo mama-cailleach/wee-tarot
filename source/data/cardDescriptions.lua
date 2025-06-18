f750ea7a232e4f83e8cdfaea05cf697562d01aa3
rachinho <marcelo.terreiro@gmail.com>
2025-06-18 16:49:41 +0100

    Added cardDescriptionsCups

--- source/data/cardDescriptions.lua
+++ source/data/cardDescriptions.lua
@@ -1,7 +1,7 @@
 
 import "data/cardDescriptionsMajor"
 import "data/cardDescriptionsWands"
---import "data/cardDescriptionsCups"
+import "data/cardDescriptionsCups"
 --import "data/cardDescriptionsSwords"
 --import "data/cardDescriptionsPentacles"
 
@@ -13,6 +13,6 @@ for k, v in pairs(CARD_DATA_MAJOR) do CARD_DATA[k] = v end
 
 -- Merge all suit tables into CARD_DATA
 for k, v in pairs(CARD_DATA_WANDS) do CARD_DATA[k] = v end
---for k, v in pairs(CARD_DATA_CUPS) do CARD_DATA[k] = v end
+for k, v in pairs(CARD_DATA_CUPS) do CARD_DATA[k] = v end
 --for k, v in pairs(CARD_DATA_SWORDS) do CARD_DATA[k] = v end
 --for k, v in pairs(CARD_DATA_PENTACLES) do CARD_DATA[k] = v end
